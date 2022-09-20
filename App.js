ec26f9eec1a75e46763f7bebb3ed40c25f54421f
ArjooReact <electronicsarzoo@gmail.com>
2022-09-20 17:09:26 +0530

    Initial commit to add

---
+++ App.js
@@ -0,0 +1,88 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import { createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import ChildComponent  from './components/ChildComponent';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+ const Stack =createStackNavigator();
+
+
+
+const App=() => {
+  
+  const fetchData = ()=>{
+    fetch('URL',{
+      type:'GET',
+      timeout: 20000,
+      params:{}
+    }).then(response =>{
+      console.log(response)
+    }).catch((error)=>{
+       console.log(error)
+    })
+
+    return JSON.stringify(response)
+  }
+
+  return (
+    <NavigationContainer>
+  
+    <Stack.Navigator initialRouteName= "CHILD"  screenOptions={{
+    
+    headerStyle: {
+     backgroundColor: '#231f20'
+   },
+   headerTintColor: '#ffffff',
+   headerTitleStyle: {
+     fontWeight: 'bold'
+   }
+   }}>
+
+<Stack.Screen name="CHILD" component={ChildComponent}  options={{
+  index:0,
+        headerShown:false
+     }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+     
+
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ android/app/src/main/java/com/reactfirstpro1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.reactfirstpro1;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Reactfirstpro1";
+  }
+}

8ebfb8b168a19c82de714e8cbe0e557a545baf0d
ArjooReact <electronicsarzoo@gmail.com>
2022-09-21 11:12:46 +0530

    Fething data in Flatlist

--- components/ChildComponent.js
+++ components/ChildComponent.js
@@ -10,21 +10,47 @@ const ChildComponent = ()=> {
   {
     name: 'Sid',
     id:2
+  },
+  {
+    name: 'yuv',
+    id:2
+  },
+  {
+    name: 'reena1',
+    id:2
+  },{
+    name: 'Sid1',
+    id:2
+  },
+  {
+    name: 'yuv1',
+    id:2
+  },
+  {
+    name: 'reena1',
+    id:2
   }
 ]
 
 
-  const renderItem = (data)=>{
-    return<View style= {{width :'100%', height:30, backgroundColor:'green'}}>
-   <Text style= {{size:20,color:'red'}}>{data.name}</Text>
+  // const renderItem = (data)=>{
+  //   return<View style= {{width :'100%', height:30, backgroundColor:'green'}}>
+  //  <Text style= {{size:20,color:'red'}}>{data.name}</Text>
+   
+  //   </View>
+  // }
+
+  const renderItem = ({ item }) => (
+    <View style= {{width :'96%', height:100, backgroundColor:'pink',padding: 10,alignSelf:'center',alignItems:'center', marginVertical: 8,marginHorizontal:6,justifyContent:'center'}}>
+   <Text style= {{fontSize:20,color:'red'}}>{item.name}</Text>
    
     </View>
-  }
+  );
     return <SafeAreaView style = {styles.background}> 
     <FlatList
     data = {data}
     renderItem = {renderItem}
-    keyExtractor = {data.id}
+    keyExtractor = {(data)=>{data.id}}
     ></FlatList>
     
    
